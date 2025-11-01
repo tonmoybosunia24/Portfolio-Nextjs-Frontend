29d18decb9a60a6b3c59c49f4fc2b4de048bd668
tonmoybosunia24 <mdtonmoybosunia24@gmail.com>
2025-11-01 21:38:03 +0600

    Navbar Section Problem Fixed

--- src/components/Common/Navbar.jsx
+++ src/components/Common/Navbar.jsx
@@ -16,23 +16,26 @@ const Navbar = () => {
     return () => window.removeEventListener('scroll', handleScroll)
   }, []);
 
-  /* ---------------------Navber Section--------------------- */
   return (
-    <nav className={`section w-full flex justify-between items-center ${scrolled ? 'fixed top-0 bg-bg-dark shadow-md duration-300 transition-all py-3 z-50' : 'bg-transparent py-3'}`}>
-      {/* --------------------Logo Section------------------ */}
-      <div>
-        <Logo></Logo>
-      </div>
-      {/* ------------------Navigation Links --------------- */}
-      <div className="hidden md:hidden lg:block">
-        <NavLinks></NavLinks>
-      </div>
-      {/* ---------------Social Links & Sidebar------------- */}
-      <div className="flex items-center gap-5">
-        {/* -----------------Social Links------------------  */}
-        <SocialLinks className="hidden lg:flex"></SocialLinks>
-        {/* ---------------------SideBars------------------- */}
-        <SideBars></SideBars>
+    /* ---------------------Navber Section--------------------- */
+    <nav className={`${scrolled ? 'fixed w-full top-0  bg-bg-dark shadow-md duration-300 transition-all py-3 z-50' : 'bg-transparent py-3'}`}>
+      {/* ------------------Navbar-Container------------------ */}
+      <div className="section flex justify-between">
+        {/* --------------------Logo Section------------------ */}
+        <div>
+          <Logo></Logo>
+        </div>
+        {/* ------------------Navigation Links --------------- */}
+        <div className="hidden md:hidden lg:block">
+          <NavLinks></NavLinks>
+        </div>
+        {/* ---------------Social Links & Sidebar------------- */}
+        <div className="flex items-center gap-5">
+          {/* -----------------Social Links------------------  */}
+          <SocialLinks className="hidden lg:flex"></SocialLinks>
+          {/* ---------------------SideBars------------------- */}
+          <SideBars></SideBars>
+        </div>
       </div>
     </nav>
   );
