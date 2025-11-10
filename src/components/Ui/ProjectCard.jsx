5286ede6492faad2ac2ec36c9b1b53e107663f3b
tonmoybosunia24 <mdtonmoybosunia24@gmail.com>
2025-11-10 23:20:56 +0600

    Project Card Design Updated

--- src/components/Ui/ProjectCard.jsx
+++ src/components/Ui/ProjectCard.jsx
@@ -1,38 +1,69 @@
+"use client";
+import { motion } from "motion/react";
 import Image from "next/image";
-import Button from "./Button";
 import Link from "next/link";
 import MotionSection from "../Common/MotionSection";
+import UnderlineAnimation from "./UnderlineAnimation";
+import { MdArrowOutward } from "react-icons/md";
 
 const ProjectCard = ({ projects }) => {
 
   return (
     /* --------------------Project Card------------------- */
-    <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-10">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10">
       {projects.map((project) => (
         /* -------------Project Card Container------------ */
-        <MotionSection className="bg-bg-dark space-y-5 rounded-2xl p-5" key={project?._id}>
+        <MotionSection className="space-y-3 md:space-y-5 lg:space-y-5 rounded-2xl" key={project?._id}>
 
           {/* --------------Image-Container-------------- */}
-          <div className="overflow-hidden rounded-xl">
-            <Image width={400} height={300} className="w-full hover:scale-105 transition duration-500" src={project?.thumbnail} alt="Project Thumbnail"></Image>
+          <div className="bg-bg-dark border-2 border-bg-white-overlay rounded-xl p-5">
+            <div className="overflow-hidden rounded-lg">
+              <Image width={600} height={400} className="w-full h-full aspect-[4/3] hover:scale-105 transition duration-500" src={project?.thumbnail} alt="Project Thumbnail"></Image>
+            </div>
           </div>
 
           {/* ------------Card-Body-Container------------ */}
-          <div className="flex justify-between items-center space-y-3">
+          <div className="flex justify-between items-center">
 
-            {/* ---------------Title-Github-------------- */}
-            <div className="space-y-2">
-              {/* ----------------Card-Title------------- */}
-              <h3 className="font-bold text-base md:text-base lg:text-2xl hover:text-pink-primary">{project?.title}</h3>
-              {/* --------------Github-Links------------- */}
-              <div className="flex gap-2">
-                <Link href={project?.githubFrontEnd} target="_blank" className="font-rubik font-semibold hover:bg-pink-primary text-gray-light hover:text-white text-xs md:text-xs lg:text-sm border-2 border-bg-white-overlay rounded-full px-2 md:px-2 lg:px-5 py-1">FrontEnd</Link>
-                <Link href={project?.githubBackEnd} target="_blank" className="font-rubik font-semibold hover:bg-pink-primary text-gray-light hover:text-white text-xs md:text-xs lg:text-sm border-2 border-bg-white-overlay rounded-full px-2 md:px-2 lg:px-5 py-1">BackEnd</Link>
-              </div>
+            <div className="space-y-1.5">
+              <UnderlineAnimation className="font-bold text-xl md:text-2xl lg:text-2xl">{project?.title}</UnderlineAnimation>
+              <p className="font-rubik text-gray-light">{project?.category}</p>
             </div>
+            {/* --------------LiveLink-Container---------- */}
+            <Link href={project?.liveLink} target="_blank" rel="noopener noreferrer">
 
-            {/* ------------------Live-Link--------------- */}
-            <Button><Link href={project?.liveLink} target="_blank">View Design</Link></Button>
+              {/* --------------Icon Container----------- */}
+              <motion.div
+                className="w-12 h-12 relative flex justify-center items-center hover:bg-pink-primary transition-colors border-2 border-bg-white-overlay rounded-lg overflow-hidden p-5 duration-500"
+                whileHover="hover"
+                initial="initial"
+              >
+
+                {/* --------------First Arrow------------ */}
+                <motion.div
+                  className="absolute"
+                  variants={{
+                    initial: { x: 0, y: 0, opacity: 1 },
+                    hover: { x: 30, y: -30, opacity: 0 },
+                  }}
+                  transition={{ duration: 0.3, ease: "easeIn" }}
+                >
+                  <MdArrowOutward className="text-2xl" />
+                </motion.div>
+                {/* --------------Second Arrow------------ */}
+                <motion.div
+                  className="absolute"
+                  variants={{
+                    initial: { x: -30, y: 30, opacity: 0 },
+                    hover: { x: 0, y: 0, opacity: 1 },
+                  }}
+                  transition={{ duration: 0.3, ease: "easeIn" }}
+                >
+                  <MdArrowOutward className="text-2xl" />
+                </motion.div>
+
+              </motion.div>
+            </Link>
 
           </div>
 
--- src/components/Ui/UnderlineAnimation.jsx
+++ src/components/Ui/UnderlineAnimation.jsx
@@ -1,7 +1,7 @@
 const UnderlineAnimation = ({ children, color = 'bg-pink-primary', className }) => {
   return (
     /* ------------------Underline Section---------------- */
-    <h3 className={`relative w-fit font-rubik hover:text-pink-primary text-base group cursor-pointer ${className}`}>
+    <h3 className={`relative w-fit font-rubik hover:text-pink-primary group cursor-pointer ${className}`}>
       {children}
       <div className={`absolute bottom-0 left-0 w-0 h-0.5 ${color} group-hover:w-full transition-all duration-300`}></div>
     </h3>
